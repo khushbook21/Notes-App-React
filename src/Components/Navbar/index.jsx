c774c32ddc6159382eb59cb873326a15570cb7b6
khushboo kanojiya <khushboo.kanojiya099@.com>
2026-07-04 00:13:36 +0530

    navbar completed

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,10 +1,11 @@
+import logo from "../../assets/note-app.png";
 export const Navbar = () => {
   return (
-    <header>
-      <div>
-        <img />
+    <header className="flex px-3 py-2 gap-3 border-b-2 border-gray-100">
+      <div className="w-12 h-12">
+        <img className="w-full h-full" src={logo} alt="logo" />
       </div>
-      <h1>NoteIt</h1>
+      <h1 className="text-indigo-500 text-4xl font-bold">NoteIt</h1>
     </header>
   );
 };
