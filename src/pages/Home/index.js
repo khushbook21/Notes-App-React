e723aab5c26f875cfa5c2291b34b0cdddd879070
khushboo kanojiya <khushboo.kanojiya099@.com>
2026-07-04 15:48:47 +0530

    removed navbar

--- src/pages/Home/index.js
+++
@@ -1,9 +0,0 @@
-import { Navbar } from "../../Components/Navbar";
-import { Fragment } from "react";
-export const Home = () => {
-  return (
-    <Fragment>
-      <Navbar />
-    </Fragment>
-  );
-};
